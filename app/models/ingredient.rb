498ec1ac5cbcaef7c039e439b666673492ffa1c7
antonin archer <antonin.archer@hec.edu>
2015-08-20 12:24:31 +0200

    associations not checked though

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ActiveRecord::Base
   has_many :doses
+  has_many :cocktails, through: :doses, :dependent => :restrict_with_exception
   validates :name, presence: true, uniqueness: true
+  before_destroy :check_for_cocktails
 end
