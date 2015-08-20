9f1d1b213140a16787fb4600e042637fea381684
antonin archer <antonin.archer@hec.edu>
2015-08-20 14:45:21 +0200

    new view

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,13 +1,27 @@
 class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show]
+
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(params[:cocktail])
+    @cocktail.save
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktails).permit(:name)
   end
 end
